40a7c3f2584c0e36abd884fa2195c4d7a222cc36
TCS <slechta@susqu.edu>
2015-12-11 10:31:06 -0500

    Uncommented all code to run full script on load.

--- SWCPM/init_dbs.php
+++ SWCPM/init_dbs.php
@@ -2,7 +2,7 @@
 
     include('includes/header.php');
     
-    //echo "Number of mats found: ".\ws::getMats()."<br/>";
-    //echo "Number of planets found: ".\ws::getPlanets()."<br/>";
-    //echo "Updating planet info: ".\ws::updatePlanets()."<br/>";
+    echo "Number of mats found: ".\ws::getMats()."<br/>";
+    echo "Number of planets found: ".\ws::getPlanets()."<br/>";
+    echo "Updating planet info: ".\ws::updatePlanets()."<br/>";
     echo "Deactivating unpassable planets: ".\ws::deactivateUnprospectablePlanets()."<br/>";
