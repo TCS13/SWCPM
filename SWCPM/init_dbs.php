e1f9d19a37dc66a36567fb0ff14714b28a1046b8
TCS <slechta@susqu.edu>
2015-12-10 21:52:53 -0500

    Allows preparing the databases with the initial data set

---
+++ SWCPM/init_dbs.php
@@ -0,0 +1,8 @@
+<?php
+
+    include('includes/header.php');
+    
+    //echo "Number of mats found: ".\ws::getMats()."<br/>";
+    //echo "Number of planets found: ".\ws::getPlanets()."<br/>";
+    //echo "Updating planet info: ".\ws::updatePlanets()."<br/>";
+    echo "Deactivating unpassable planets: ".\ws::deactivateUnprospectablePlanets()."<br/>";
