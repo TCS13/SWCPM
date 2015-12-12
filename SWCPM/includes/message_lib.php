5d8b187ab5a9974c5edd13a6c887e3db5a54f72f
TCS <slechta@susqu.edu>
2015-12-11 22:06:38 -0500

    Added error messages and display.

---
+++ SWCPM/includes/message_lib.php
@@ -0,0 +1,45 @@
+<?php
+class message {
+
+    public static function dispMsg($msgNumber) {
+        $msgNumber = stripslashes($msgNumber);
+        $msg = null;
+        $err = 0;
+        if ($msgNumber == '1') {
+            $msg = "A required parameter was not supplied.";
+            $err = 1;
+        } elseif ($msgNumber == '2') {
+            $msg = "The specified planet does not exist.";
+            $err = 1;
+        } elseif ($msgNumber == '3') {
+            $msg = "The specified raw material does not exist.";
+            $err = 1;
+        } elseif ($msgNumber == '4') {
+            $msg = "The deposit size was invalid.";
+            $err = 1;
+        } elseif ($msgNumber == '5') {
+            $msg = "The x-coordinate was invalid.";
+            $err = 1;
+        } elseif ($msgNumber == '6') {
+            $msg = "The y-coordinate was invalid.";
+            $err = 1;
+        } elseif ($msgNumber == '7') {
+            $msg = "You have added a new deposit to this planet.";
+        } elseif ($msgNumber == '8') {
+            $msg = "Adding the deposit failed for an unspecified reason.";
+	    $err = 1;
+        } else
+	{
+	    $msg = "The error you received was unidentifiable.<br/>Please contact a site administrator with how you received this error.";
+	    $err = 1;
+	}
+        if ($err == 1) {
+            return '<table><tr><td><font color="red">' . $msg . '</font></td></tr></table>';
+        } else {
+            return '<table><tr><td><font color="green">' . $msg . '</font></td></tr></table>';
+        }
+    }
+
+}
+
+?>
